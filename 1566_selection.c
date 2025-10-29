a581dc50a5037faff1c7528c02823f883486b5cd
tilopz <tiagopachecolopes@gmail.com>
2025-10-29 16:54:03 -0300

    1566_selection.c

---
+++ 1566_selection.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void Selecao (int n, int v[]) {
+    int i, j, min, x;
+    for (i = 0; i < n-1; i++) {
+        min = i;
+        for (j = i+1; j < n; j++) {
+            if (v[j] < v[min]) min = j;
+        }
+        x = v[i];
+        v[i] = v[min];
+        v[min] = x;
+    }
+}
+
+int main() {
+    int NC, i;
+    scanf("%d", &NC);
+    if (NC >= 100){
+        printf("Erro na quantidade de casos\n");
+        return 1;
+    }
+
+    for (i = 0; i < NC; i++){
+        int N, j;
+        scanf("%d", &N);
+        if (N <= 1 || N > 3000000){
+            printf("Erro no tamanho do vetor\n");
+            return 1;
+        }
+        int v[N];
+        for (j = 0; j < N; j++){
+            scanf("%d", &v[j]);
+            if (v[j] < 20 || v[j] > 230){
+                printf("Erro na altura\n");
+                return 1;
+            }
+        }
+        Selecao(N, v);
+        for (j = 0; j < N; j++){
+            printf("%d ", v[j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
